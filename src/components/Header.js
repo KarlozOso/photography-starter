b626dad0fc41d9d42ffa2b70c17573f7c291cea7
KarlozOso <135464400+KarlozOso@users.noreply.github.com>
2023-08-19 06:20:52 +0000

    Need to fix errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Socials from './Socials';
+import MobileNav from './MobileNav'
+import Logo from '../img/header/logo.svg'
+import {Link} from "react-router-dom"
+
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>Header</div>;
 };
 
 export default Header;
